e548a0eff0506522d923576f8282c5bcf0515b91
Thukten Norbu <thuktennorbu33@gmail.com>
2022-05-05 00:10:17 +0600

    Added Flutter

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'screens/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tutorial',
+      theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          primarySwatch: Colors.blue),
+      home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+  dynamic li = [];
+  void _incre() {
+    count++;
+    print(count);
+  }
+
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Blue"),
+        backgroundColor: Colors.grey,
+      ),
+      body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  color: Colors.red,
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.amber,
+                        ),
+                        child: const Text('Pull'),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (BuildContext context) {
+                            return const Login();
+                          }));
+                        },
+                      ),
+                      Image.asset('Images/download.png'),
+                      ElevatedButton(
+                        child: const Text('Push'),
+                        onPressed: () {},
+                      ),
+                      Image.asset('Images/CMjr.png'),
+                    ],
+                  ),
+                )
+              : const SizedBox()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          size: 24,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.help), label: "Help"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
